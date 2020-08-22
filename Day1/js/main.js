53d5aa9f3f3a84665e51ccd03552df28ccbb3b9f
Jawad Elmzabi <elmzabi90@gmail.com>
2020-08-22 13:20:53 +0100

    add random mode selection feature

--- Day1/js/main.js
+++ Day1/js/main.js
@@ -13,20 +13,37 @@ var mode_items = document.querySelectorAll(
 
 var modeSelected = false;
 
-for (let i = 0; i < mode_items.length; i++) {
+for (let i = 0; i < mode_items.length; i++) { // loop 3la Ga3 mode_items 
   const element = mode_items[i];
 
+  // add click event && remove "selcted" class from all element (then add "selected" class to cliked element)
+  // check element if has a mode data attr then pick random element & store it mode
+  // store attributes [path, extension] in local storage
+  // add "selected" class to cliked element
+  // set modeSelected = true to use it to check if any mode was selected when we click start game
   element.addEventListener("click", function (params) {
+    // store mode info 
+    var mode;
+
     for (let i = 0; i < mode_items.length; i++) {
       const element = mode_items[i];
       element.classList.remove("selected");
     }
 
+
+    if (this.dataset.mode) {
+      var new_ar = Array.from(mode_items).slice(0, -1); // convert Nodelist to an array and slice(0, -1) for 0 to Before the last
+
+      mode = new_ar[Math.floor(Math.random() * new_ar.length)];
+    } else {
+      mode = element;
+    }
+
     localStorage.setItem(
       "mode",
       JSON.stringify({
-        path: element.dataset.modePath,
-        ex: element.dataset.modeEx,
+        path: mode.dataset.modePath,
+        ex: mode.dataset.modeEx,
       })
     );
     this.classList.add("selected");
@@ -39,3 +56,11 @@ document.getElementById("start__btn").addEventListener("click", function (e) {
     e.preventDefault();
   }
 });
+
+// ila kan random mode if element has data attr mode
+// random between 0 and lenght - 2 bach n7ydu random elemnt 
+// ++ use array.filter()
+
+// =======================
+//        For Testing
+// =======================
